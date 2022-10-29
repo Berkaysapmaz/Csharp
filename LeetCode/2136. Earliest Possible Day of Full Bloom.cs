9fcc7b8689b6e3ea71385ce67aacd500ac58cf20
Berkaysapmaz <berkay30082000@hotmail.com>
2022-10-30 01:17:43 +0300

    Create 2136. Earliest Possible Day of Full Bloom.cs

---
+++ LeetCode/2136. Earliest Possible Day of Full Bloom.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int EarliestFullBloom(int[] plantTime, int[] growTime) {
+         
+      /*You have n flower seeds. Every seed must be planted first before it can begin to grow, then bloom. Planting a seed takes time and so does the growth of a seed. You are given two 0-indexed integer arrays plantTime and growTime, of length n each:
+
+plantTime[i] is the number of full days it takes you to plant the ith seed. Every day, you can work on planting exactly one seed. You do not have to work on planting the same seed on consecutive days, but the planting of a seed is not complete until you have worked plantTime[i] days on planting it in total.
+growTime[i] is the number of full days it takes the ith seed to grow after being completely planted. After the last day of its growth, the flower blooms and stays bloomed forever.
+From the beginning of day 0, you can plant the seeds in any order.
+
+Return the earliest possible day where all seeds are blooming.*/
+      
+              int i = growTime.Length - 1;
+              Array.Sort(growTime, plantTime);
+              int sum = plantTime[i];
+              int lastAdd = growTime[i];      
+              i = i-1;
+              while(i >= 0)
+              {
+                    sum += plantTime[i];
+                    if(lastAdd > plantTime[i]+growTime[i])
+                    {
+                      lastAdd = lastAdd - plantTime[i];
+                    }
+                    else
+                    {
+                      lastAdd = growTime[i];
+                    }
+                    i--;     
+              }            
+              return sum+lastAdd;
+    }
+}
