0cbea2df0ad199e5a7536533a91a90e24d2026eb
Berkaysapmaz <berkay30082000@hotmail.com>
2022-11-04 01:43:10 +0300

    Create 709. To Lower Case.cs

---
+++ LeetCode/709. To Lower Case.cs
@@ -0,0 +1,6 @@
+public class Solution {
+    public string ToLowerCase(string s) {
+      /*Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.*/  
+      return s.ToLower();
+    }
+}
